4c0cf852dfb8f745231d92f976a445a3273f5dc7
Diego Mejía <undiegomejia@users.noreply.github.com>
2021-05-25 15:57:03 -0600

    Settings JS

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const settingContainer = document.querySelector('.settings')
+const btnSetting = document.querySelectorAll('.button-settings')
+
+const showSetting = function() {
+    settingContainer.classList.toggle('showed')
+}
+
+btnSetting.forEach(function(e) {
+    e.addEventListener('click', function(e) {
+        e.preventDefault();
+        showSetting()
+    })
+})
